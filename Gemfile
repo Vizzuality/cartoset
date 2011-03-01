26519e12b12bcda9c0a21814e52b95f35f84162c
Fernando Espinosa <fer@ferdev.com>
2011-03-01 18:09:28 +0100

    Here we go!

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+
+group :development, :test do
+  gem 'ruby-debug19', :require => 'ruby-debug', :platforms => :mri_19
+end
+
+group :test do
+  gem 'capybara'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'steak'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cartoset::Application.config.secret_token = '9abd9384eb9295f25f6e453477984269f91de653ab0dcd8dd4f3f4dd22183b6c7d324fc5a0e2ed50e490e30e5d2c05179752ae7ef391f0abf8b091180cfe2b0b'
