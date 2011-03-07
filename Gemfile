44e27ccd5c876b791e22c192068abde26a869008
Fernando Blat <ferblape@gmail.com>
2011-03-07 10:31:18 +0100

    Testing authentication

--- Gemfile
+++ Gemfile
@@ -9,11 +9,9 @@ gem 'rails_warden'
 
 group :development, :test do
   gem 'ruby-debug19', :require => 'ruby-debug', :platforms => :mri_19
-end
-
-group :test do
   gem 'capybara'
   gem 'rspec'
   gem 'rspec-rails'
   gem 'steak'
+  gem 'launchy'
 end
---
+++ spec/acceptance/acceptance_helper.rb
@@ -0,0 +1,35 @@
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
+require "steak"
+require "capybara/rails"
+require "capybara/dsl"
+require "selenium-webdriver"
+
+# Put your acceptance spec helpers inside /spec/acceptance/support
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+Capybara.default_driver    = :selenium
+Capybara.default_wait_time = 10
+Capybara.server_port = 3333
+
+RSpec.configure do |config|
+
+  config.include Warden::Test::Helpers
+  config.include Capybara, :type => :acceptance
+
+  config.before(:each) do
+    Rails.cache.clear
+  end
+
+  config.after(:each) do
+    case page.driver.class
+    when Capybara::Driver::RackTest
+      page.driver.rack_mock_session.clear_cookies
+    when Capybara::Driver::Culerity
+      page.driver.browser.clear_cookies
+    when Capybara::Driver::Selenium
+      page.driver.cleanup!
+    end
+    Capybara.use_default_driver
+  end
+
+end
