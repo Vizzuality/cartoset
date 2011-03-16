20628b83acc4887476af8c6312f2fcef3f0ef655
dcano <dcano@vizzuality.com>
2011-03-16 20:22:50 +0100

    First version of the register process

---
+++ app/controllers/register_controller.rb
@@ -0,0 +1,20 @@
+class RegisterController < ApplicationController
+  include FeatureGetterHelper
+  layout 'register'  
+
+  def step0
+  end
+
+  def step1
+  end
+
+  def step2
+  end
+
+  def step3
+  end
+
+  def step4
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Cartoset::Application.routes.draw do
 
   match '/admin' => 'admin/dashboard#index'
 
+  match '/register' => 'register#step0'
+  match '/register/step1' => 'register#step1'
+  match '/register/step2' => 'register#step2'
+  match '/register/step3' => 'register#step3'
+  match '/register/step4' => 'register#step4'
+
   root :to => "home#index"
   match "/auth/:provider/callback" => "sessions#create"
 
