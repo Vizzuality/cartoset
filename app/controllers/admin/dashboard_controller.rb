2ce3e3681530b969de2b86a11d8c2af13b3e3c6d
Fernando Espinosa <fer@ferdev.com>
2011-09-01 10:31:36 +0200

    Fix button link 'or edit them in CartoDB'

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,8 @@ class Admin::DashboardController < Admin::AdminController
     @features_columns = Feature.data_columns
     @features         = Feature.page(1).per_page(1)
     @pages            = Cartoset::Config['pages'].select{|p| p['id'].present? } || []
+    @cartodb_host = CartoDB::Settings['host']
+    @table_id = CartoDB::Connection.table(Cartoset::Config['features_table']).try(:id)
   end
 
 end
