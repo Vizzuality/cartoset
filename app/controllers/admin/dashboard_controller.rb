ce2b20b954227148e514df2a3e44266f89ee4c98
Fernando Espinosa <fer@ferdev.com>
2011-09-01 19:09:02 +0200

    Fix pagination in dashboard's features table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,13 @@
 class Admin::DashboardController < Admin::AdminController
 
   def index
+    @page             = (params[:page] || 1).to_i
     @features_columns = Feature.data_columns
-    @features         = CartoDB::Connection.query("SELECT * FROM #{Cartoset::Config['features_table']}", :page => 1, :rows_per_page => 1).rows
+    @features_count   = CartoDB::Connection.query("SELECT COUNT(cartodb_id) as features_count FROM #{Cartoset::Config['features_table']}").rows.first.features_count
+    @features         = CartoDB::Connection.query("SELECT * FROM #{Cartoset::Config['features_table']}", :page => @page, :rows_per_page => 10).rows
     @pages            = Cartoset::Config['pages'].select{|p| p['id'].present? } || []
-    @cartodb_host = CartoDB::Settings['host']
-    @table_id = CartoDB::Connection.table(Cartoset::Config['features_table']).try(:id)
+    @cartodb_host     = CartoDB::Settings['host']
+    @table_id         = CartoDB::Connection.table(Cartoset::Config['features_table']).try(:id)
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,17 @@
 module ApplicationHelper
   def paginate(rows, current_page, total_count, per_page = 10)
 
+    window      = 2
     total_pages = (total_count.to_f / per_page.to_f).ceil
-    pages = (1..total_pages).to_a
+    last_page   = total_pages - 1
+
+    left  = (current_page - 1).downto(1).to_a.first(window).sort
+    right = (current_page + 1).upto(total_pages).to_a.first(window)
+
+    pages = 1.upto(current_page - 1).to_a.map{|p| left.include?(p) || p == 1 ? p : '...'}.uniq + [current_page] + (current_page + 1).upto(last_page).to_a.map{|p| right.include?(p) || p == last_page ? p : '...'}.uniq
 
     content_tag :div, :class => 'pagination' do
-      raw pages.map{|page| link_to page, '#', :class => page == current_page ? 'current' : nil}.join(' ')
+      raw pages.map{|page| page.eql?('...') ? content_tag(:span, page) : link_to(page, admin_path(:page => page), :class => page == current_page ? 'current' : nil)}.join(' ')
     end
   end
 end
