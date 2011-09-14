efe89d9daac4329103b16dbf30c35b8da2dfb64d
Javier de la Torre <jatorre@gmail.com>
2011-09-14 08:16:27 -0600

    Fixed some issues when using Polygon datasets

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -4,13 +4,13 @@ class FeaturesController < ApplicationController
   before_filter :get_feature, :only => [:show]
 
   def index
-    table = CartoDB::Connection.query "SELECT cartodb_id, title, the_geom FROM #{features_table_name} LIMIT 20"
+    table = CartoDB::Connection.query "SELECT cartodb_id, title, ST_Centroid(the_geom) as the_geom FROM #{features_table_name} LIMIT 20"
     @features = table.rows || []
     @features_json = @features.map{|f| {:title => f.title, :latitude => f.the_geom.y, :longitude => f.the_geom.x}}.to_json.html_safe
   end
 
   def show
-
+    debugger
   end
 
 end
